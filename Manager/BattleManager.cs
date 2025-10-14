d5383e2ecdc2a92f542562877834be9b7fa5dff4
shin0112 <wnslcosltimo12@gmail.com>
2025-10-14 22:18:28 +0900

    feat: 싱글톤 전투 매니저 클래스 생성하기

---
+++ Manager/BattleManager.cs
@@ -0,0 +1,10 @@
+﻿namespace TEXT_RPG.Manager
+{
+    internal class BattleManager
+    {
+        private static BattleManager _instance;
+        public static BattleManager Instance { get; } = _instance ??= new BattleManager();
+
+        public List<object> Monsters = [];
+    }
+}
