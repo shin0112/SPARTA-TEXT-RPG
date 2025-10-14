442e77e7849db9b51bd261cafa871859bf9cc8cf
shin0112 <wnslcosltimo12@gmail.com>
2025-10-14 21:31:12 +0900

    feat: 전투 화면 렌더링 만들기

--- Scenes/BattleScene.cs
+++ Scenes/BattleScene.cs
@@ -4,7 +4,28 @@
     {
         public void Show()
         {
-            Console.WriteLine("Battle!!");
+            // player 정보 가져오기 (임시 데이터)
+            int level = 1;
+            string name = "이름";
+            string job = "직업";
+            int currentHp = 100;
+            int maxHp = 100;
+
+            Console.WriteLine("Battle!!\n");
+
+            // todo: 몬스터 리스트
+            Console.WriteLine("몬스터 리스트 ...\n");
+
+            Console.WriteLine("[내 정보]");
+            Console.WriteLine($"Lv. {level} {name} ({job})");
+            Console.WriteLine($"체력 {currentHp}/{maxHp}\n");
+
+            Console.WriteLine("1. 공격");
+            Console.WriteLine();
+
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">> ");
+            Console.Read();
         }
     }
 }
