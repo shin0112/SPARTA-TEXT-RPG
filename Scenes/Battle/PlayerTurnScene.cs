10a3f3299813e3df0451f5a857625bd44bf68857
shin0112 <wnslcosltimo12@gmail.com>
2025-10-15 00:28:06 +0900

    feat: 플레이어 턴 화면 만들고 BattleSceneBase 상속하기

---
+++ Scenes/Battle/PlayerTurnScene.cs
@@ -0,0 +1,10 @@
+﻿namespace TEXT_RPG.Scenes.Battle
+{
+    internal class PlayerTurnScene : BattleSceneBase
+    {
+        protected override void HandleInput()
+        {
+            // 입력 처리
+        }
+    }
+}
