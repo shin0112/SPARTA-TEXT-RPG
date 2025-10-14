c17274712026f33b8e154ea9cd140df8e9d4ee84
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-14 20:24:30 +0900

    feat monster.cs

---
+++ Monster.cs
@@ -0,0 +1,11 @@
+﻿namespace TEXT_RPG
+{
+    public class Monster
+    {
+        public string name;
+        public int level;
+        public int atk;
+        public int def;
+        public int hp;
+    }
+}
