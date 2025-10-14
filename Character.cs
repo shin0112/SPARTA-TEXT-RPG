3c329c07631bfc9eb1c2c012edb10c793c17cf4f
Gayeong-you <yko66901@gmail.com>
2025-10-14 19:58:40 +0900

    Character (캐릭터 스크립트입니다. 기본 백수캐릭터이고, 추후에 다른 팀원분께서 필요하시다면 얼마든지 수정 가능합니다.)
     qqㅂㅐㅌㅂㅋ

---
+++ Character.cs
@@ -0,0 +1,14 @@
+namespace TEXT_RPG;
+
+public class Character
+{
+    public int level = 1; //레벨
+    public string name = "백수"; //직업
+    public int atk = 5; //공격력
+    public int def = 10; //방어력
+    public int hp = 10; //체력
+    public int mentalhp = 15; //정신력
+    public int gold = 100; //재력
+    public string experience = "아직 없습니다";
+
+}
