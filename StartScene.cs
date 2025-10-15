c5d5ae5ada961e95aa4a77ce88a3a90aa6c53522
shin0112 <wnslcosltimo12@gmail.com>
2025-10-15 15:55:16 +0900

    feat: 배틀 화면 start scene이랑 연결하기

--- StartScene.cs
+++ StartScene.cs
@@ -31,6 +31,7 @@
                 else if (input == "2")
                 {
                     Console.WriteLine("사회에 뛰어들기를 선택했습니다");
+                    new BattleStartScene().Show();
                 }
                 else if (input == "3")
                 {
