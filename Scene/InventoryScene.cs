751a42a4e4a457c4290a4de921f80686c927c531
haramey <40591849+haramey123@users.noreply.github.com>
2025-10-15 20:56:27 +0900

    Feat: 인벤토리 씬 작성, 장착관리 씬 생성
    1. 인벤토리 씬에서 아이템 인스턴스들을 리스트를 통해 나열함
    2. 장착관리 씬은 생성만 해둠

---
+++ Scene/InventoryScene.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEXT_RPG.Core;
+using TEXT_RPG.Repository;
+
+namespace TEXT_RPG.Scene
+{
+    internal class InventoryScene
+    {
+        public void Inventory()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("인벤토리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n\n");
+                Console.WriteLine("[아이템 목록]\n");
+
+                // 아이템 리스트 나열 반복문
+                foreach (var item in ItemRepository.InventoryItem)
+                {
+                    string isEquip = item.IsEquipped ? "[E] " : "    "; // 각 장비 장착 여부 표시
+                    Console.WriteLine($" - {isEquip}{item.Name}\t| {item.Type} +{item.Value}\t| {item.Description}");
+                }
+
+                Console.WriteLine("\n1. 장착 관리\n0. 나가기\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">>> ");
+
+                string input = Console.ReadLine() ?? "";
+
+                if (input == "1")
+                {
+                    new EquipManagementScene().EquipManagement();
+                }
+                else if (input == "0")
+                {
+
+                    break;
+                }
+            }
+        }
+    }
+}
