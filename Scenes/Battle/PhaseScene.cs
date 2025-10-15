32cec187df5b21acbf7248f0525ee30aff5ec553
shin0112 <wnslcosltimo12@gmail.com>
2025-10-15 19:26:37 +0900

    feat: 전투 phase 한 턴 진행 로직 제작 및 더미데이터에 실제 몬스터 값 적용

--- Scenes/Battle/PhaseScene.cs
+++ Scenes/Battle/PhaseScene.cs
@@ -6,17 +6,40 @@ namespace TEXT_RPG.Scenes.Battle
     internal class PhaseScene : BattleSceneBase
     {
         // Todo: Monster 정보 저장하기 (임시 데이터)
-        private Monster monster = BattleManager.Instance.Monsters[BattleManager.Instance.MonsterNumber - 1];
+        private List<Monster> monsters = BattleManager.Instance.Monsters;
 
         public override void Show()
+        {
+            PlayerTurn();
+            foreach (var monster in monsters)
+            {
+                if (monster.IsDead) continue;
+                MonsterTurn(monster);
+            }
+        }
+
+        private void PlayerTurn()
         {
             Console.WriteLine("Battle!\n");
 
             // Todo: player 정보 넣기
+            Monster monster = monsters[BattleManager.Instance.MonsterNumber - 1];
+
             Console.WriteLine($"{"플레이어"}의 공격!");
             Console.WriteLine($"Lv. {monster.Level} {monster.Name} 을(를) 맞췄습니다.\n");
 
             ShowMonsterInfo();
+            HandleSelections();
+        }
+
+        private void MonsterTurn(Monster monster)
+        {
+            Console.WriteLine("Battle!\n");
+
+            Console.WriteLine($"Lv. {monster.Level} {monster.Name}의 공격!");
+            Console.WriteLine($"{"플레이어"} 을(를) 맞췄습니다. [데미지:{monster.Stats.Atk}]");
+
+            ShowPlayerInfo();
             HandleSelections();
         }
 
@@ -27,24 +50,28 @@ namespace TEXT_RPG.Scenes.Battle
 
         protected override void HandleInput(int select)
         {
-
         }
 
         protected override void ShowMonsterInfo()
         {
+            Monster monster = monsters[BattleManager.Instance.MonsterNumber - 1];
             Console.WriteLine($"Lv. {monster.Level} {monster.Name}");
 
-            // Todo: monster의 isDead로 변경
-            // Todo: 실제 monster 체력으로 변경
-            bool isDead = true;
-            if (isDead)
+            // Todo: 플레이어 공격력 가져오기
+            int playerAtk = 10;
+            int beforeHp = monster.Stats.Hp;
+
+            monster.TakeDamage(playerAtk);
+
+            if (monster.IsDead)
             {
-                Console.WriteLine($"HP {10} → Dead\n");
+                Console.WriteLine($"HP {beforeHp} → Dead\n");
             }
             else
             {
-                Console.WriteLine($"HP {10} → {"나중 체력"}\n");
+                Console.WriteLine($"HP {beforeHp} → {monster.Stats.Hp}\n");
             }
+
         }
     }
 }
