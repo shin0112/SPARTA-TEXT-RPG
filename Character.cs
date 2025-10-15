28caf67eded8086f104af95b30f291e3f8d656d6
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-15 11:55:12 +0900

    merge Char

--- Character.cs
+++ Character.cs
@@ -1,14 +1,34 @@
+
 namespace TEXT_RPG;
 
-public class Character
+public class Spec
 {
-    public int level = 1; //레벨
-    public string name = "백수"; //직업
-    public int atk = 5; //공격력
-    public int def = 10; //방어력
-    public int hp = 10; //체력
-    public int mentalhp = 15; //정신력
-    public int gold = 100; //재력
-    public string experience = "아직 없습니다";
+    public void Specification()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("스펙 보기");
+            Console.WriteLine("캐릭터의 스펙이 표시됩니다.");
 
+            Console.WriteLine($"level"); //캐릭터 스크립트랑 추후에 연결 후 코드 수정해야함
+            
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
+            
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                //캐릭터 스크립트랑 연결
+            }
+            else if (input == "0")
+            {
+                Console.Clear();
+                //스타트씬 스크립트와 연결
+            }
+        }
+    }
+}
 }
