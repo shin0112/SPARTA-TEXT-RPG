7a654268c788df0411e2420e88dab672fdeae515
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-15 11:40:07 +0900

    feat Stats

--- Monster.cs
+++ Monster.cs
@@ -5,12 +5,14 @@
         public string Name { get; set; }
         public int Level { get; set; }
         public bool isDead { get; set; }
+        public Stats Stats { get; set; }
 
-        public Monster(string name, int level)
+        public Monster(string name, int level, Stats stats)
         {
             Name = name;
             Level = level;
             isDead = false;
+            Stats = stats;
         }
 
         public void Attack(IAttack target)
@@ -21,7 +23,7 @@
             }
             else
             {
-
+                
             }
         }
     }
