356aad758f437e0b8d0297410f0dd7723a8008c8
jiwon <checkmate0710@gmail.com>
2025-10-16 16:16:16 +0900

    [feat] 상점 화면 씬, 상점 구매 씬 추가 및 상점 화면 ver.1 완성
    제곧내

---
+++ Scene/ShopBuyScene.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TEXT_RPG.Scene
+{
+    internal class ShopBuyScene
+    {
+        
+        //ShopScene shopscene = new ShopScene();
+        //public void DisplayBuyShop()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine(.shopIntroText1);
+        //    ShopItemList();
+        //    Console.WriteLine(shopIntroText2);
+        //    Console.Write(">> ");
+        //}
+    }
+}
