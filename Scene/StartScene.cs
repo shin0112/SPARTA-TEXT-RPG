9c6463aa548b84739d21dff81ed11eb6ad7e194e
Gayeong-you <yko66901@gmail.com>
2025-10-16 17:27:34 +0900

    feat. 스타트씬 수정
    대사 수정하고 깔끔해보이게 다듬음. 상점이동 부분은 다시 수정해야합니다. 나머지는 전부 정상동작합니다.

--- Scene/StartScene.cs
+++ Scene/StartScene.cs
@@ -8,30 +8,28 @@ namespace TEXT_RPG.Scene
         public void GameStart()
         {
             Console.Clear();
-            Console.WriteLine("거친 정글에 뛰어든 건 나지만 힘들다..");
-            Console.WriteLine("나..무사히 취업 할 수 있는거냐..");
-
+            Console.WriteLine("이제부터 시작이야!");
+            Console.WriteLine("강해져서 최고의 개발자가 되겠어");
+            Console.WriteLine();
             Console.WriteLine("1.스펙보기");
             Console.WriteLine("2.사회에 뛰어들기");
             Console.WriteLine("3.인벤토리");
             Console.WriteLine("4.상점 이동");
-
             Console.WriteLine("0.집으로 돌아간다..");
-
+            Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
             string input = Console.ReadLine();
 
             if (input == "1")
             {
                 Console.WriteLine("스펙보기를 선택했습니다.");
                 GameManager.Instance.SceneInfo = SceneType.Spec;
-                //new SpecScene().Specification(); 
             }
             else if (input == "2")
             {
                 Console.WriteLine("사회에 뛰어들기를 선택했습니다");
                 GameManager.Instance.SceneInfo = SceneType.Battle;
-                //new BattleStartScene().Show();
             }
             else if (input == "3")
             {
@@ -43,9 +41,16 @@ namespace TEXT_RPG.Scene
             {
                 Console.WriteLine("상점으로 이동합니다.");
             }
+            else if (input == "0")
+            {
+                Console.WriteLine("벌써 돌아가시려구요?");
+                Console.ReadKey();
+                return;
+            }
             else
             {
                 Console.WriteLine("잘못 입력하셨습니다.");
+                Console.ReadKey();
             }
         }
     }
