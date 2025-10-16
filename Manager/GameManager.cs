2698cfe93868683b0669d550418a8b55b830e19c
shin0112 <wnslcosltimo12@gmail.com>
2025-10-16 14:09:10 +0900

    feat: player 정보 받아서 game manager에 저장하기

--- Manager/GameManager.cs
+++ Manager/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleRPG;
+using TEXT_RPG.Core;
 using TEXT_RPG.Scene;
 
 namespace TEXT_RPG.Manager
@@ -8,6 +9,7 @@ namespace TEXT_RPG.Manager
         public Intro StartIntro = new();
         private static GameManager _instance = new();
         public static GameManager Instance => _instance;
+        public Player? Player { get; set; }
 
         // 게임 시작하는 함수
         // 실행 로직을 변경하고 싶다면 이 함수를 수정해주세요.
