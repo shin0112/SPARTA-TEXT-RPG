0cd6be0cf69a237e4a6a06a00f75543ffbb0e21e
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-16 19:38:46 +0900

    feat. 플레이어도 회피!!

--- Core/Player.cs
+++ Core/Player.cs
@@ -38,6 +38,12 @@
         }
         public void TakeDamage(int damage)
         {
+            int evadeRate = random.Next(0, 100);
+            if (evadeRate <= 10)
+            {
+                Console.WriteLine("공격이 빗나갔습니다.");
+                return;
+            }
             int actualDamage = Stats.TakeDamage(damage);
             Console.WriteLine($"{Name} 이(가) {actualDamage} 의 피해를 입었습니다.");
         }
