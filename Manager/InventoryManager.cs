28fba244bbc01c1f79ac43135842983a382c296a
haramey <40591849+haramey123@users.noreply.github.com>
2025-10-17 12:09:19 +0900

    Feat: 인벤토리 매니저 싱글톤 구현
    인벤토리 매니저 싱글톤화 및 초기화
    
    참고: 하승우 튜터님께 여쭤본 결과, 싱글톤을 "게임 매니저" 하나만 만들고 그 안에다가 "배틀 매니저"와 "인벤토리 매니저"를 넣는걸 선호한다고 하십니다.
    이래야 나중에 싱글톤 관리가 편하다고 하시네요.
    하지만 저희는 일단 싱글톤을 나눈 채로 진행해보는걸로...

---
+++ Manager/InventoryManager.cs
@@ -0,0 +1,26 @@
+﻿using TEXT_RPG.Core;
+using TEXT_RPG.Repository;
+
+namespace TEXT_RPG.Manager
+{
+    internal class InventoryManager
+    {
+        // 싱글톤
+        private static InventoryManager _instance = new();
+        public static InventoryManager Instance => _instance;
+
+
+        // 리포지터리 복제
+        private readonly ItemRepository itemRepository = new();
+
+        public List<Item> InventoryItem;
+        public List<Item> ShopItem;
+
+        private InventoryManager()
+        {
+            InventoryItem = itemRepository.InventoryItem;
+            ShopItem = itemRepository.ShopItem;
+        }
+
+    }
+}
