7a1f38f2e01b94c99e5310abe86124583c6a0a2b
shin0112 <wnslcosltimo12@gmail.com>
2025-10-17 19:07:09 +0900

    chore: 던전에 출현하는 몬스터 리스트를 던전 번호화 매칭하기
    {던전번호, 던전에 출현하는 몬스터 리스트{일반 몬스터들, 특수 몬스터들}}

--- Repository/MonsterRepository.cs
+++ Repository/MonsterRepository.cs
@@ -4,13 +4,15 @@ namespace TEXT_RPG.Repository
 {
     internal class MonsterRepository
     {
-        public List<Monster> MonstersNo1 => [
+        public Dictionary<int, List<List<Monster>>> DungeonMonsters { get; }
+
+        public List<Monster> MonstersNo1 { get; } = [
             new Monster("Unity 훑어보기", 1, new Stats(5, 1, 5)),
             new Monster("C# 기본", 2, new Stats(7, 2, 10)),
             new Monster("C# 심화", 3, new Stats(9, 3, 10)),
         ];
 
-        public List<Monster> SpecialMonsterNo1 => [
+        public List<Monster> SpecialMonsterNo1 { get; } = [
             new Monster("김영호 튜터", 5, new Stats(5, 5, 20)),
             new Monster("신찬용 튜터", 5, new Stats(5, 5, 20)),
             new Monster("이재현 튜터", 5, new Stats(5, 5, 20)),
@@ -21,10 +23,19 @@ namespace TEXT_RPG.Repository
             new Monster("진수인 매니저", 5, new Stats(5, 5, 20)),
         ];
 
-        public List<Monster> MonstersNo2 => [
+        public List<Monster> MonstersNo2 { get; } = [
             new Monster("머리 까진 면접관", 4, new Stats(4, 4, 20)),
             new Monster("내가 말하는데 조는 면접관", 5, new Stats(5, 5, 25)),
             new Monster("막말하는 면접관", 6, new Stats(6, 6, 30)),
         ];
+
+        public MonsterRepository()
+        {
+            DungeonMonsters = new Dictionary<int, List<List<Monster>>>
+            {
+                { 1, new() { MonstersNo1, SpecialMonsterNo1 } },
+                { 2, new() { MonstersNo2 } }
+            };
+        }
     }
 }
