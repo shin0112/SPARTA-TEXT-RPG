3f317c67a16ec8b40b950c374a87d4beae3704f8
jiwon <checkmate0710@gmail.com>
2025-10-17 19:06:37 +0900

    [feat] 하람님 코드 Merge, 멘트 수정, 상점 구매 시 player gold 추가 코드 작성
    제곧내입니다.

--- Scene/ShopBuyScene.cs
+++ Scene/ShopBuyScene.cs
@@ -9,9 +9,8 @@ namespace TEXT_RPG.Scene
 
     internal class ShopBuyScene : ShopScene
     {
-        private readonly ItemRepository itemRepository = new();
+        public List<Item> ShopItem = InventoryManager.Instance.ShopItem;
 
-        public List<Item> ShopItem = [];
         public int ShopItemNumber { get; set; } = 0;
 
         public bool hasEnoughGold = false;
@@ -23,13 +22,13 @@ namespace TEXT_RPG.Scene
         public string shopIntroText3 = @$"
 
 
-구매할 아이템의 번호를 입력하세요. ex) ""2""입력 시 ""기계식 키보드""를 구매합니다.
+구매할 아이템의 번호를 입력하세요. ex) '2' 입력 시 '기계식 키보드'을(를) 구매합니다.
 
 0. 뒤로가기
 
 
 
-멀뚱멀뚱 서서 뭐 하고 있어? 언넝 골라~"; // 아이템 리스트 변경 시 예시 체크 필요
+""멀뚱멀뚱 서서 뭐 하고 있어? 언넝 골라~"""; // 아이템 리스트 변경 시 예시 체크 필요
 
 
 
@@ -49,29 +48,33 @@ namespace TEXT_RPG.Scene
 
             if (ShopItem[i].IsBuy == true)
             {
-                Console.WriteLine("그 장비는 이미 판매되었다네!"); // 추후 구매화면에서 즉시 구매가 안 되게 처리하면 else문 삭제 예정
+                Console.WriteLine("\"그 장비는 이미 판매되었다네!\""); // 추후 구매화면에서 즉시 구매가 안 되게 처리하면 else문 삭제 예정
                 return null;
             }
 
                 if (ShopItem[i].Price > GameManager.Instance.Player.Gold)
             {
-                Console.WriteLine("자네... 돈은 있지?");
+                Console.WriteLine("\"자네... 돈은 있지? 아니, 이걸로 되겠어? 참.\"");
+                Console.WriteLine("돈을 더 벌어오자.");
+                return null;
             }
 
             Console.WriteLine("정말 구매하시겠습니까?");
-            Console.WriteLine("1. 예.");
+            Console.WriteLine("1. 예");
             Console.WriteLine("2. 조금만 더 고민해보자...");
             inputBuyCheck = Console.ReadLine();
 
-            if (input != "1")
+            if (inputBuyCheck != "1")
             {
                 Console.WriteLine("아이템을 구매하지 않습니다.");
+                return null;
             }
 
             if (ShopItem[i].IsBuy == false)
             {
                 ShopItem[i].IsBuy = true;
-                Console.WriteLine("좋은 선택일세. 사회에서 마음껏 뛰놀게.");
+                GameManager.Instance.Player.Gold -= ShopItem[i].Price;
+                Console.WriteLine("\"좋은 선택일세. 사회에 나가 꿈을 펼쳐보게나.\"");
             }
 
             return ShopItem[i]; // <- 객체 그대로 반환
