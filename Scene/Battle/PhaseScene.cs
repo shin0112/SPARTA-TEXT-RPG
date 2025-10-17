9c983c20598d90d7a1151120fb04ac6211bddcb5
shin0112 <wnslcosltimo12@gmail.com>
2025-10-18 01:31:48 +0900

    refactor: player, monster 턴 부분 BattleManager에게 책임 위임

--- Scene/Battle/PhaseScene.cs
+++ Scene/Battle/PhaseScene.cs
@@ -1,6 +1,5 @@
 ﻿using TEXT_RPG.Core;
 using TEXT_RPG.Manager;
-using TEXT_RPG.UI;
 
 namespace TEXT_RPG.Scene.Battle
 {
@@ -15,51 +14,21 @@ namespace TEXT_RPG.Scene.Battle
             Player player = GameManager.Instance.Player!;
 
             // 플레이어 턴
-            PlayerTurn(player);
+            ShowTitle();
+            BattleManager.Instance.PlayerTurn();
+            HandleSelections();
 
             // 몬스터 턴 
             foreach (var monster in monsters)
             {
                 if (monster.IsDead) continue; // 죽은 몬스터인 경우 스킵
-                if (player.IsDead) // 플레이어 죽는 경우 패배 처리
-                {
-                    BattleManager.Instance.Defeat();
-                    return;
-                }
-                MonsterTurn(monster, player);
+                ShowTitle();
+                BattleManager.Instance.MonsterTurn(monster);
+                HandleSelections();
             }
 
             // 한 턴 종료
             BattleManager.Instance.TurnEnd();
-        }
-
-        private void PlayerTurn(Player player)
-        {
-            ShowTitle();
-            Monster monster = monsters[BattleManager.Instance.MonsterNumber - 1];
-
-            Console.WriteLine($"{player.Name}의 공격!");
-            Console.WriteLine($"Lv. {monster.Level} {monster.Name} 을(를) 맞췄습니다.\n");
-
-            ShowMonsterInfo();
-            HandleSelections();
-        }
-
-        private void MonsterTurn(Monster monster, Player player)
-        {
-            ShowTitle();
-            Console.WriteLine($"Lv. {monster.Level} {monster.Name}의 공격!");
-
-            int actualDamage = monster.Stats.Atk - player.Stats.Def;
-            if (actualDamage < 0) actualDamage = 0;
-
-            Console.WriteLine($"{player.Name} 을(를) 맞췄습니다. [데미지:{actualDamage}]");
-
-            monster.Attack(player);
-
-            Console.WriteLine();
-            BattleSceneUI.ShowPlayerInfo();
-            HandleSelections();
         }
 
         protected override void HandleInput(int select)
