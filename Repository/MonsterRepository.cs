a08458393a1d34ca2ca2b0c3ae7fae8b6f84ef18
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-17 20:47:16 +0900

    refactor 몬스터 레포지토리

--- Repository/MonsterRepository.cs
+++ Repository/MonsterRepository.cs
@@ -4,13 +4,13 @@ namespace TEXT_RPG.Repository
 {
     internal class MonsterRepository
     {
-        public List<Monster> MonstersNo1 => [
+        public List<Monster> MonstersNo1 { get; } = [
             new Monster("Unity 훑어보기", 1, new Stats(5, 1, 5), new Reward(1, 10, new List<int>{ })),
             new Monster("C# 기본", 2, new Stats(7, 2, 10), new Reward(2, 20, new List<int>{ })),
             new Monster("C# 심화", 3, new Stats(9, 3, 10), new Reward(3, 30, new List<int>{ })),
         ];
 
-        public List<Monster> SpecialMonstersNo1 => [
+        public List<Monster> SpecialMonstersNo1 { get; } = [
             new Monster("김영호 튜터", 5, new Stats(5, 5, 20), new Reward(5, 50, new List<int>{1})),
             new Monster("신찬용 튜터", 5, new Stats(5, 5, 20), new Reward(5, 50, new List<int>{1})),
             new Monster("이재현 튜터", 5, new Stats(5, 5, 20), new Reward(5, 50, new List<int>{1})),
@@ -21,7 +21,7 @@ namespace TEXT_RPG.Repository
             new Monster("진수인 매니저", 5, new Stats(5, 5, 20), new Reward(5, 50, new List<int>{8})),
         ];
 
-        public List<Monster> MonstersNo2 => [
+        public List<Monster> MonstersNo2 { get; } = [
             new Monster("머리 까진 면접관", 4, new Stats(4, 4, 20), new Reward(4, 40, new List<int>{ })),
             new Monster("내가 말하는데 조는 면접관", 5, new Stats(5, 5, 25), new Reward(5, 50, new List<int>{ })),
             new Monster("막말하는 면접관", 6, new Stats(6, 6, 30), new Reward(6, 60, new List<int>{ })),
