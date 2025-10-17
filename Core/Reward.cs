d5c72841f1a2df80cdb3212e8e747e72694ae6b6
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-17 10:17:12 +0900

    feat. 보상 클래스 추가

---
+++ Core/Reward.cs
@@ -0,0 +1,30 @@
+﻿using TEXT_RPG.Repository;
+
+namespace TEXT_RPG.Core
+{
+    internal class Reward
+    {
+        public int Exp { get; private set; }
+        public int Gold { get; private set; }
+        public List<Item> DropItem { get; private set; }
+
+        public Reward(int exp, int gold, List<Item> drop)
+        {
+            Exp = exp;
+            Gold = gold;
+            DropItem = drop;
+        }
+
+        public void Add(Reward other)
+        {
+            Exp += other.Exp;
+            Gold += other.Gold;
+            
+        }
+
+        public void Get(Player player)
+        {
+
+        }
+    }
+}
